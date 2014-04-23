df0ebe9dad79fca122e5a605075f433a86389f45
Guilherme <guilhermedelima@gmail.com>
2014-04-23 17:32:16 -0300

    Criando cifrador de Vernam

--- questao-1/include/cipher.hpp
+++ questao-1/include/cipher.hpp
@@ -1,28 +1,26 @@
 #ifndef CIPHER_HPP
 #define CIPHER_HPP
+#include <string>
+
+using namespace std;
+
+class Cipher{
+	protected:
+		string key;
+	public:
+		Cipher(string key);
+		virtual string cipherText(const string& text) const = 0;
+		virtual string decodeText(const string& cripto) const = 0;
+};
 
 
-
-
-
-
-
-
-
-
-
-
-
+class VernamCipher : Cipher{
+	public:
+		VernamCipher(string key);
+		string cipherText(const string& text) const;
+		string decodeText(const string& cripto) const;
+};
 
 
 
 #endif
-
-
-
-
-
-
-
-
-
--- questao-1/src/test_cipher.cpp
+++ questao-1/src/test_cipher.cpp
@@ -1,12 +1,25 @@
+#include <iostream>
+#include <string>
 #include "cipher.hpp"
 
 using namespace std;
 
 int main(void){
 
+	string key;
+	string text;
+	string cripto;
 
+	key = "CHAVEVERNAMCRIPTOGRAFIA";
+	text = "Texto Em claro para cifracao ANO 2014";
 
+	VernamCipher cipher(key);
+	
+	cripto = cipher.cipherText(text);
 
+	cout << text << endl << endl;
+	cout << cripto << endl << endl;
+	cout << cipher.decodeText(cripto) << endl;
 
 	return 0;
 }
