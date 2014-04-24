44a8a0b7b376018a416cd5d5ecd7fda67c691275
Tiago Pereira <tiagogomespereira@gmail.com>
2014-04-24 09:07:37 -0300

    Entradas por arquivo.

--- questao-1/src/test_cipher.cpp
+++ questao-1/src/test_cipher.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "cipher.hpp"
 
 using namespace std;
 
-int main(void){
-
+int main(int argc, char *argv[]){
+    
+    ifstream itext;
+    ifstream ikey;
+    
 	string key;
 	string text;
 	string cripto;
 
-	key = "CHAVEVERNAMCRIPTOGRAFIA";
-	text = "Texto Em claro para cifracao ANO 2014";
-
+    cout << argc << endl;
+    
+    if(argc != 3){
+       key = "CHAVEVERNAMCRIPTOGRAFIA";
+	   text = "Texto Em claro para cifracao ANO 2014";
+    } else {
+        itext.open(argv[1]);
+        ikey.open(argv[2]);
+        
+        getline(ikey,key);
+        getline(itext,text);
+    }
+    
 	VernamCipher cipher(key);
 	
 	cripto = cipher.cipherText(text);
